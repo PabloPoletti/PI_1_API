38918b2a3874e8527156fcf39740e31e5a44fac2
PabloPoletti <104991677+PabloPoletti@users.noreply.github.com>
2022-09-15 08:59:30 -0300

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 
-engine =create_engine('mysql://ba9afcb304b59e:27ca6703@us-cdbr-east-06.cleardb.net/heroku_339b76dfa1a5759?reconnect=true')
+CONEXION = 'mysql://ba9afcb304b59e:27ca6703@us-cdbr-east-06.cleardb.net/heroku_339b76dfa1a5759'
+
+engine =create_engine(CONEXION)
 
 meta = MetaData()
 
