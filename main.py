5eda14cd40037cf3cfa573882d8cfa871d4fab9c
PabloPoletti <104991677+PabloPoletti@users.noreply.github.com>
2022-09-15 19:06:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ app = FastAPI()
 @app.get('/races')
 def get_races():
     return conn.execute(select(func.count(t_races2.c.raceId).label('Cantidad_de_Carreras'),
-    t_races2.c.year).group_by(t_races2.c.year).order_by(desc('Cantidad_de_Carreras'))).first()
+    (t_races2.c.year).label('Año con Mayor Cantidad de Carreras')).group_by(t_races2.c.year).order_by(desc('Cantidad_de_Carreras'))).first()
 
 @app.get('/Piloto')
 def get_Piloto():
-    return conn.execute(select(t_results.c.driverId, t_drivers.c.driverRef.label('Piloto'),
+    return conn.execute(select(t_drivers.c.driverRef.label('Piloto Con Mayor Cantidad de Primeros Puestos'),
         func.count(t_results.c.positionOrder).label('Cantida_de_Primeros_Puestos')).
         join(t_drivers, t_results.c.driverId==t_drivers.c.driverId).where(t_results.c.positionOrder==1).
         group_by(t_results.c.driverId).order_by(desc('Cantida_de_Primeros_Puestos'))).first()
 
 @app.get('/Circuito')
 def get_Circuito():
-    return conn.execute(select(func.count(t_races2.c.circuitId), t_races2.c.name).
+    return conn.execute(select((t_races2.c.name).label('Circuito con Mayor Cantidad de Carreras'), (func.count(t_races2.c.circuitId)).label('Cantidad De Carreras')).
     group_by(t_races2.c.circuitId).order_by(desc(func.count(t_races2.c.circuitId)))).first()
 
 
 
 @app.get('/MejorPiloto')
 def get_MejorPiloto():
-    return conn.execute(select(func.sum(t_results.c.points).label('Puntos'), t_drivers.c.driverRef.label('Piloto')).
+    return conn.execute(select((t_drivers.c.driverRef).label('Piloto Con Mayor Cantidad de Puntos corriendo con escuderias Britanica o Americana'),func.sum(t_results.c.points).label('Cantidad de Puntos')).
         join(t_drivers, t_results.c.driverId==t_drivers.c.driverId).
         join(t_constructors, t_results.c.constructorId==t_constructors.c.constructorId).
         where(or_(t_constructors.c.nationality=='British',t_constructors.c.nationality=='American')).
